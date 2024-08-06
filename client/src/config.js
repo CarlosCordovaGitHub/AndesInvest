a1b7b5564b891360fcfb4dfb54430b18a73315b4
Carlos Córdova <168382165+CarlosCordovaGitHub@users.noreply.github.com>
2024-08-05 22:33:15 -0500

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL|| "https://andes-invest-api.vercel.app/api";
+export const API_URL = import.meta.env.VITE_API_URL|| "https://andes-invest-api.vercel.app";
