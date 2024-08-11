1f538b035ca466e849e0f6809644602f8cfbea19
Carlos Córdova <168382165+CarlosCordovaGitHub@users.noreply.github.com>
2024-08-11 13:03:01 -0500

    Update config.js

--- server/src/config.js
+++ server/src/config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 // Configurar dotenv para cargar variables de entorno
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
-export const PORT = process.env.PORT || 4000;
+export const PORT = process.env.PORT || 10000;
 export const MONGODB_URI = process.env.MONGODB_URI;
 export const TOKEN_SECRET = process.env.TOKEN_SECRET;
 export const FRONTEND_URL = process.env.FRONTEND_URL;
