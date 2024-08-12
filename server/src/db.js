dcd13f6ae4bec5cc6b0a93b97d4e3d853b738104
Carlos Córdova <168382165+CarlosCordovaGitHub@users.noreply.github.com>
2024-08-11 21:13:53 -0500

    Update db.js

--- server/src/db.js
+++ server/src/db.js
@@ -3,9 +3,14 @@ import { MONGODB_URI } from "./config.js";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 20000, // Tiempo de espera hasta seleccionar un servidor (20 segundos)
+      socketTimeoutMS: 20000, // Tiempo de espera para las operaciones de socket (20 segundos)
+    });
     console.log("MongoDB is connected");
   } catch (error) {
-    console.error(error);
+    console.error("Error connecting to MongoDB:", error);
   }
 };
